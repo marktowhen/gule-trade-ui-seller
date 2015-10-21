2611f6e8d07baded830c4030d6bdbbf61adb28cd
Mike Cao <michael-cao@outlook.com>
2015-10-21 10:45:57 +0800

    init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jingyunetradebackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jingyunetradebackApp
+ */
+angular.module('jingyunetradebackApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
