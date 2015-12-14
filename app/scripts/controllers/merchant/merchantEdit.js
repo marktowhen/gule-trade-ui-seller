d02dc2906e45afb239cf7b2538fe4ebe9ce088c0
liuguang <liuguang@jingyunbank.com>
2015-12-14 14:48:56 +0800

    liug-20151214

--- app/scripts/controllers/merchant/merchantEdit.js
+++ app/scripts/controllers/merchant/merchantEdit.js
@@ -9,11 +9,37 @@
  */
 shopbackApp.controller('MerchantEditController', function ($scope,$state,$route,$location,MerchantEditService) {
 	var mid = $state.params.mid;//获取商家ID
+	$scope.m = { merchantName: "",
+				 merchantEname:"",
+				 merchantCode:"",
+				 merchantAddress:"",
+				 merchantScale:"",
+				 employeeNum:"",
+				 tel:"",
+				 zipcode:"",
+				 qq:"",
+				 adminSortNum:"",
+				 invoiceFlag:"0",
+				 level:"",
+				 merchantDesc:""
+	};
 	if(mid){
 		MerchantEditService.getMerchantInfo(mid)
 				.success(function(data){
 					var r = data.body;
 					$scope.m.merchantName = r.merchantName;
+					$scope.m.merchantEname = r.merchantEname;
+					$scope.m.merchantCode = r.merchantCode;
+					$scope.m.merchantAddress = r.merchantAddress;
+					$scope.m.merchantScale = r.merchantScale;
+					$scope.m.employeeNum = r.employeeNum;
+					$scope.m.tel = r.tel;
+					$scope.m.zipcode = r.zipcode;
+					$scope.m.qq = r.qq;
+					$scope.m.adminSortNum = r.adminSortNum;
+					$scope.m.invoiceFlag = r.invoiceFlag;
+					$scope.m.level = r.level;
+					$scope.m.merchantDesc = r.merchantDesc;
 				});
 	}
 });
