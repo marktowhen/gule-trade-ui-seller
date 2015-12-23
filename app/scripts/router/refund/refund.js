dd75f065d5228e3f5c83c2b0264c0d9f5d026870
haiting <zhanghaiting@jingyunbank.com>
2015-12-23 10:22:44 +0800

    功能分类整理

--- app/scripts/router/refund/refund.js
+++ app/scripts/router/refund/refund.js
@@ -11,24 +11,24 @@
 shopbackApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $stateProvider
       //品牌列表查询
-      .state('seller-center.refunding-list',{
-        templateUrl: '/views/seller-center/refund-management/refunding.html',
+      .state('trading-center.refunding-list',{
+        templateUrl: '/views/trading-center/refund-management/refunding.html',
         url:"/refunding"
       })//品牌维护
-      .state('seller-center.returning-list',{
-        templateUrl:'/views/seller-center/refund-management/returning.html',
+      .state('trading-center.returning-list',{
+        templateUrl:'/views/trading-center/refund-management/returning.html',
         url:"/returning"
       })//品牌新建
-      .state('seller-center.refunded-list',{
-        templateUrl:'/views/seller-center/refund-management/refunded.html',
+      .state('trading-center.refunded-list',{
+        templateUrl:'/views/trading-center/refund-management/refunded.html',
         url:"/refunded"
       })
-      .state('seller-center.refunding-accept', {
-        templateUrl:'/views/seller-center/refund-management/refunding_receiver.html',
+      .state('trading-center.refunding-accept', {
+        templateUrl:'/views/trading-center/refund-management/refunding_receiver.html',
         url:"/refunding/accept/:rid"
       })
-      .state('seller-center.refunding-deny', {
-        templateUrl:'/views/seller-center/refund-management/refunding_deny.html',
+      .state('trading-center.refunding-deny', {
+        templateUrl:'/views/trading-center/refund-management/refunding_deny.html',
         url:"/refunding/deny/:rid"
       });
   });
